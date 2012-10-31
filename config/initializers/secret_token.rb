317a2fc9aabc36d9192879479070c3dde1e6225e
Len Weincier <len@cloudafrica.net>
2012-10-31 13:47:19 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T4a::Application.config.secret_token = '79f46eede495c9e5c9287b6055c10eca127c5f136c67ffb66b0e72febaf8de9797769334d6c58606355b9d876b9dc37447881308f4dc8505979e03cdd89c3658'
